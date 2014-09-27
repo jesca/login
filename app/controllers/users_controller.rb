3097930097a36777333bb1e6f8fbb3a7965660e7
jesca <jesca116@gmail.com>
2014-09-26 20:08:25 -0700

    start functional tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,6 +28,29 @@ class UsersController < ApplicationController
     render :json => { :errCode => code}
   end
 
+    """
+    Output should look like this: 
+
+    # Running:
+
+    ..........
+
+    Finished in 0.049780s, 200.8839 runs/s, 281.2374 assertions/s.
+
+    10 runs, 14 assertions, 0 failures, 0 errors, 0 skips
+    """
+
+#http://www.ruby-doc.org/core-2.1.2/String.html#method-i-lines
+ def unitTests
+      # get line first, split by ",", get number
+      rakeTest = `rake test`
+      console = rakeTest.split("\n")[-1].split(",")
+      totalTests = console[0].split(" ")[0].scan(/\d+/)[0]
+      failedTests = console[console.length-1].scan(/\d+/)[0]
+      render :json => {:totalTests => Integer(totalTests), :nrFailed => Integer(failedTests), :output => rakeTest}
+  end
+
+
   private 
   def user_params
     params.require(:user).permit(:username, :password)
